b903694ba97e120b5e1d292e05d1b73971c15ead
R.W.H.M. Thulani Amanda <129428203+Thulani0@users.noreply.github.com>
2023-12-16 06:18:35 +0530

    mem_wb_latch update

--- RISCV32I_pipelined_project/RISCV32I_pipelined_project.srcs/sources_1/new/MEM-WB_latch.sv
+++ RISCV32I_pipelined_project/RISCV32I_pipelined_project.srcs/sources_1/new/MEM-WB_latch.sv
@@ -1,40 +1,65 @@
 `timescale 1ns / 1ps
 module mem_wb_latch#(
-        parameter ADDRESS_LENGTH = 32
+        parameter ADDRESS_LENGTH = 32,
+        parameter INSTR_LENGTH = 5
     )(
         input logic clk_mem_wb,
         input logic rst_mem_wb,
-        input logic flush_mem_wb
+        input logic flush_mem_wb,
 //INPUT
     //INPUT control signals
         //WB_control_signals
-
+    input logic MemToReg_in_mem_wb,
+    input logic RegWrite_in_mem_wb,
     //INPUT data
-        //WB_data       
+        //WB_data
+        input logic [ADDRESS_LENGTH-1:0] ReadData_in_mem_wb,
+        input logic [ADDRESS_LENGTH-1:0]ALUresult_in_mem_wb,
+        input logic [INSTR_LENGTH-1:0] Inst7_11_in_mem_wb,       
 
 
 //OUTPUT 
      //OUTPUT control signals
         //WB_control_signals
+    output logic MemToReg_out_mem_wb,
+    output logic RegWrite_out_mem_wb,
     //OUTPUT data
-        //WB_data 
+        //WB_data         
+        output reg [ADDRESS_LENGTH-1:0]ReadData_out_mem_wb,
+        output reg [ADDRESS_LENGTH-1:0]ALUresult_out_mem_wb,
+        output reg [INSTR_LENGTH-1:0]Inst7_11_out_mem_wb
+    
         
     );
     
     always_ff @(posedge clk_mem_wb) begin
     if (rst_mem_wb) begin
         // Reset all outputs to default values
+        MemToReg_out_mem_wb <=0;
+        RegWrite_out_mem_wb <=0;
+        ReadData_out_mem_wb <=0;
+        ALUresult_out_mem_wb <=0;
+        Inst7_11_out_mem_wb <=0;
 
     end
     else if (flush_mem_wb) begin
         // Reset control signals, but leave data intact
-
+        MemToReg_out_mem_wb <=0;
+        RegWrite_out_mem_wb <=0;
 
         // Data lines will get the next value
+        ReadData_out_mem_wb <= ReadData_in_mem_wb;
+        ALUresult_out_mem_wb <= ALUresult_in_mem_wb;
+        Inst7_11_out_mem_wb <= Inst7_11_in_mem_wb;
 
     end
     else begin
         // Normal operation: Transfer input to output
+        MemToReg_out_mem_wb <= MemToReg_in_mem_wb;
+        RegWrite_out_mem_wb <= RegWrite_in_mem_wb;
+        ReadData_out_mem_wb <= ReadData_in_mem_wb;
+        ALUresult_out_mem_wb <= ALUresult_in_mem_wb;
+        Inst7_11_out_mem_wb <= Inst7_11_in_mem_wb;
 
     end
 end
--- RISCV32I_pipelined_project/RISCV32I_pipelined_project.srcs/sources_1/new/MEM_stage.sv
+++ RISCV32I_pipelined_project/RISCV32I_pipelined_project.srcs/sources_1/new/MEM_stage.sv
@@ -12,19 +12,19 @@ module MEM_stage#(
     input logic  MemRead,
     input logic  MemWrite,
     //outputs of ExMem latch
-    input logic ExMem_Inst,
-    input logic ExMem_WB,
+//    input logic ExMem_Inst,
+//    input logic ExMem_WB,
     
     //outputs of data memory 
-    output logic [DATA_WIDTH-1:0]  ReadData,
+    output logic [DATA_WIDTH-1:0]  ReadData
     //inputs for MemWb latch
-    output logic MemWb_Inst,
-    output logic MemWb_WB
+//    output logic MemWb_Inst,
+//    output logic MemWb_WB
     );
     
     
     DataMem dataMem(clk,ExMem_Addr,WriteData,MemRead,MemWrite,ReadData);
-    My_Mem_Wb_Ltc my_mem_wb_ltc(clk,ExMem_Addr,ExMem_Inst,ExMem_WB,ReadData,MemWb_Addr,MemWb_Inst,MemWb_WB,MemWb_Rd);
+//    My_Mem_Wb_Ltc my_mem_wb_ltc(clk,ExMem_Addr,ExMem_Inst,ExMem_WB,ReadData,MemWb_Addr,MemWb_Inst,MemWb_WB,MemWb_Rd);
     
 endmodule
 
